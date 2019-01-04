54191995e521ceb9aea9664a3a26403e60b32e6f
Nolan T Yoo <nolan@projectexist.net>
2019-01-04 04:31:26 -0800

    shift to 3d

--- GamePackage/Assets/Games/EndlessRunner/Scripts/Player/EndlessRunnerPlayer.cs
+++ GamePackage/Assets/Games/EndlessRunner/Scripts/Player/EndlessRunnerPlayer.cs
@@ -2,37 +2,75 @@
 using UnityEngine;
 using UnityEngine.Experimental.Input;
 
+// Demonstrates nested MonoBehavior, new InputSystem
+
 public class EndlessRunnerPlayer : SingletonBehavior<EndlessRunnerPlayer>
 {
+    #region Properties
+    [Header("Components/Children/Etc")]
     public Rigidbody RB;
     public Camera Camera;
-    public InputAction JumpAction;
+    public InputActionAsset MovementAsset;
     [Header("Body Parts")]
     public GameObject LFoot;
     public GameObject RFoot;
+    [Header("Runtime variables")]
+    public float DistanceTravelled = 0f;
 
     private EndlessRunnerPlayerFeet _lFootScript;
     private EndlessRunnerPlayerFeet _rFootScript;
+    private Vector3 _initialPosition;
 
+    private InputAction _jumpAction;
+    private InputAction _moveAction;
+    private float _movement = 0f;
+    #endregion
+
+    #region Lifecycle
     protected override void Awake()
     {
         base.Awake();
-        this.RB.velocity = new Vector3(5, 0, 0);
+        this.RB.velocity = new Vector3(0, 0, 5);
 
         this._lFootScript = this.LFoot.AddComponent<EndlessRunnerPlayerFeet>();
         this._rFootScript = this.RFoot.AddComponent<EndlessRunnerPlayerFeet>();
+        this._initialPosition = this.transform.position;
 
-        this.JumpAction.performed += _ => this.tryJump();
+        this._jumpAction = this.MovementAsset.actionMaps[0].actions[0];
+        this._jumpAction.performed += _ => this.tryJump();
+        this._moveAction = this.MovementAsset.actionMaps[0].actions[1];
+        this._moveAction.performed += this.tryMove;
     }
 
     private void OnEnable()
     {
-        this.JumpAction.Enable();
+        this._jumpAction.Enable();
+        this._moveAction.Enable();
     }
 
     private void OnDisable()
     {
-        this.JumpAction.Enable();
+        this._jumpAction.Disable();
+        this._moveAction.Disable();
+    }
+
+    private void Update()
+    {
+        Vector3 pos = this.transform.position;
+        pos.x += this._movement * Time.deltaTime;
+        this.transform.position = pos;
+
+        this.DistanceTravelled = this.transform.position.z - this._initialPosition.z;
+
+        SDbgPanel.Log("Player Velocity", this.RB.velocity);
+        SDbgPanel.Log("Player Distance", this.DistanceTravelled);
+    }
+    #endregion
+
+    #region Helpers
+    private void tryMove(InputAction.CallbackContext input)
+    {
+        this._movement = input.ReadValue<float>();
     }
 
     private void tryJump()
@@ -45,11 +83,6 @@ public class EndlessRunnerPlayer : SingletonBehavior<EndlessRunnerPlayer>
         }
     }
 
-    private void Update()
-    {
-        SDbgPanel.Log("Player Velocity", this.RB.velocity);
-    }
-
     public class EndlessRunnerPlayerFeet : MonoBehaviour
     {
         public float Distance = 0.005f;
@@ -60,4 +93,5 @@ public class EndlessRunnerPlayer : SingletonBehavior<EndlessRunnerPlayer>
             return Physics.Raycast(this.transform.position, Vector3.down, out hit, this.Distance);
         }
     }
+    #endregion
 }
